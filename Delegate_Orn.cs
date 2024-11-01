4d2c45c5937f503259bb254c5b12b152b9fe7167
Limtotage <abdullahltgyalim@gmail.com>
2024-11-01 23:32:36 +0300

    Create Delegate_Orn.cs

---
+++ Delegate_Orn.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Delegate_Orn
+{
+    public delegate void MesajGoster(string mesaj);
+    //Yukarıdaki MesajGoster delegate türü, void döndüren ve string parametre alan tüm metodları temsil eder.
+    public static void EkranaYazdir(string mesaj)
+    {
+        Console.WriteLine("Mesaj: " + mesaj);
+    }
+
+    public static void Main()
+    {
+        // Delegate örneği oluşturma ve bir metot atama
+        MesajGoster mesajGoster = EkranaYazdir;
+
+        // Delegate'i çalıştırma
+        mesajGoster("Merhaba, dunya!");
+    }
+}
