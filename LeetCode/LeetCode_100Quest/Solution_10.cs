99939e2b0eaff64af9163fd77da4bd91fefd18d8
Limtotage <abdullahltgyalim@gmail.com>
2024-12-24 08:03:06 +0300

    Create Solution_10.cs

---
+++ LeetCode/LeetCode_100Quest/Solution_10.cs
@@ -0,0 +1,14 @@
+public class Solution_10 {
+    public int SearchInsert(int[] nums, int target) {
+        int left = 0, right = nums.Length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return nums.Contains(target)?left-1:left;
+    }
+}
