8b8f12deed89f4874b672c20a8636232b72e7674
Limtotage <abdullahltgyalim@gmail.com>
2025-01-23 08:37:54 +0300

    Create Solution_24.cs

---
+++ LeetCode/LeetCode_100Quest/Solution_24.cs
@@ -0,0 +1,14 @@
+public class Solution_24 {
+    public int result;
+    public int ClimbStairs(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        var dp = new int[n+1];
+        dp[0]=dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+}
